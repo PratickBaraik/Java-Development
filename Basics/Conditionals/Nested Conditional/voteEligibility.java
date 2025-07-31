f6d8162e9c8b94af0643ff4ab98ef6691239e79e
Pratick Baraik <animeNerd.int@protonmail.com>
2025-07-31 20:20:25 +0530

    implemented nested if-else style for vote eligibility

---
+++ Basics/Conditionals/Nested Conditional/voteEligibility.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class voteEligibility {
+    public static void main(String[] args) {
+        // defining scanner object for taking input
+        Scanner takeInput = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = takeInput.nextInt();
+
+        if(age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            if(age >= 16) {
+                System.out.println("You can pre-register to vote.");
+            } else {
+                System.out.println("You are too young for voting.");
+            }
+        }
+    }
+}
