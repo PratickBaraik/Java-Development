5f56d4bab40950ff6481c6d49e9789ed390646a7
Pratick Baraik <animeNerd.int@protonmail.com>
2025-07-31 20:36:02 +0530

    update vote eligibility java code

--- Basics/Conditionals/Nested Conditional/voteEligibility.java
+++ Basics/Conditionals/Nested Conditional/voteEligibility.java
@@ -8,14 +8,16 @@ public class voteEligibility {
         System.out.print("Enter your age: ");
         int age = takeInput.nextInt();
 
-        if(age >= 18) {
+        if (age >= 18) {
             System.out.println("You are eligible to vote.");
         } else {
-            if(age >= 16) {
+            if (age >= 16) {
                 System.out.println("You can pre-register to vote.");
             } else {
                 System.out.println("You are too young for voting.");
             }
         }
+
+        takeInput.close();
     }
 }
